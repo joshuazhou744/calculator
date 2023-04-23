ba0df966a4a844cfa2ea72218083e3f5b694e221
joshuazhou744 <128266380+joshuazhou744@users.noreply.github.com>
2023-04-23 12:23:55 -0600

    Revamp code and complete calculator

--- calc.js
+++ calc.js
@@ -1,79 +1,131 @@
+class Calculator {
+    constructor(last, current) {
+        this.last = last
+        this.current = current
+        this.erase()
+    }
+
+    erase() {
+        this.currentOperand = ''
+        this.lastOperand = ''
+        this.operation = undefined
+    }
+    
+    del() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+    
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+    
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.lastOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.lastOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+    
+    compute() {
+        let computation
+        const prev = parseFloat(this.lastOperand);
+        const ongoing = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(ongoing)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + ongoing;
+                break
+            case '-':
+                computation = prev - ongoing;
+                break
+            case '*':
+                computation = prev * ongoing;
+                break
+            case '/':
+                computation = prev / ongoing;
+                break
+            default: 
+                return
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.lastOperand = '';
+    }
+    
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+          integerDisplay = ''
+        } else {
+          integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`
+        } else {
+          return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.current.innerText = 
+            this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.last.innerText = `${this.getDisplayNumber(this.lastOperand)} ${this.operation}`
+        } else {
+            this.last.innerText = '';
+        }
+    }
+};
+
 // VARIABLES
-const display = document.querySelector('#display')
-const clear = document.querySelector('.clear');
-const sign = document.querySelector('.sign');
-const percent = document.querySelector('.percent');
-const decimal = document.querySelector('.decimal');
-const equals = document.querySelector('.equals');
-const operands = document.querySelectorAll('.operand');
-const operators = document.querySelectorAll('.operator');
-operandArray = [...operands];
-operatorArray = [...operators];
+const numberButtons = document.querySelectorAll('[data-num]');
+const operationButtons = document.querySelectorAll('[data-op]');
+const equalButton = document.querySelector('[data-equal]');
+const deleteButton = document.querySelector('[data-del]');
+const clear = document.querySelector('[data-clear]');
+const last = document.querySelector('[data-last]');
+const current = document.querySelector('[data-current]');
+
+const calculator = new Calculator(last, current);
 
 // FUNCTIONS
 
-// INPUTS
-
-equals.addEventListener('click', () => {
-    evaluate()
-});
-
-decimal.addEventListener('click', () => {
-    dot()
-});
-
-percent.addEventListener('click', () => {
-    percentage()
-});
-
-sign.addEventListener('click', () => {
-    changeSign()
-});
-
-clear.addEventListener('click', () => {
-    erase()
-});
-
-operatorArray.forEach((item) => {
-    item.addEventListener('click', () => {
-        operation(item.textContent);
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
     });
 });
 
-operandArray.forEach((item) => {
-    item.addEventListener('click', () => {
-        input(item.textContent);
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
     });
 });
 
-// OUTPUTS
+equalButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
 
-function input(operand) {
-    display.innerHTML = operand;
-};
+clear.addEventListener('click', button => {
+    calculator.erase();
+    calculator.updateDisplay();
+});
 
-function operation(operator) {
-    alert(operator);
-};
+deleteButton.addEventListener('click', button => {
+    calculator.del();
+    calculator.updateDisplay();
+});
 
-function erase() {
-    alert('erase');
-};
-
-function dot() {
-    alert('decimal');
-};
-
-function changeSign() {
-    alert('sign');
-};
-
-function percentage() {
-    alert('percent');
-};
-
-function evaluate() {
-    alert('eval');
-};
 
 // MAIN CODE
+  
